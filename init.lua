5111e8a299321d9b39e51e497a75bd079fb66e13
Stvk imension <76146430+oong819@users.noreply.github.com>
2022-10-25 23:59:11 +0700

    test

--- init.lua
+++ init.lua
@@ -2,4 +2,23 @@
 -- =====================
 -- See LICENSE.txt for licensing and README.md for other information.
 
-modname = {}
+	minetest.register_decoration({
+		name = "stvk:a",
+		deco_type = "schematic",
+		place_on = {"default:dry_dirt_with_dry_grass"},
+		sidelen = 16,
+		noise_params = {
+			offset = 0.024,
+			scale = 0.015,
+			spread = {x = 250, y = 250, z = 250},
+			seed = 2,
+			octaves = 3,
+			persist = 0.66
+		},
+		biomes = {"deciduous_forest"},
+		y_max = 31000,
+		y_min = 1,
+		schematic = minetest.get_modpath("default") .. "/schematics/apple_tree.mts",
+		flags = "place_center_x, place_center_z",
+		rotation = "random",
+	})
